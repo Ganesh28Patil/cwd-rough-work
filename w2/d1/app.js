0e03e5d1acfb6179add89f620499ea563b236006
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2023-02-26 10:32:05 +0530

    w2 d1

--- w2/d1/app.js
+++ w2/d1/app.js
@@ -1,3 +1,8 @@
 var num = 10;
 console.log('num: ', num);
 console.warn('num: ', num);
+function add(a, b) {
+    return a + b;
+}
+var sum = add("10", 20);
+console.log('sum:', sum);
--- w2/d1/app.ts
+++ w2/d1/app.ts
@@ -1,3 +1,9 @@
-const num:number = 10;
+let num:number = 10;
 console.log('num: ', num)
 console.warn('num: ', num)
+function add(a:number,b:number){
+    return a+b;
+}
+
+var sum = add("10",20);
+console.log('sum:', sum);
